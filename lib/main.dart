638b6de8bc8b3676293eb88677d5ae3d86b7bae4
adogbeji <baka_1993@yahoo.co.uk>
2025-02-22 08:34:54 +0000

    Add phone input field in phone login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chat_app/views/screens/auth/phone_login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fast Chat App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +29,13 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => const PhoneLogin(),
+      },
     );
   }
 }
--- lib/views/screens/auth/phone_login.dart
+++ lib/views/screens/auth/phone_login.dart
@@ -12,9 +12,32 @@ class _PhoneLoginState extends State<PhoneLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
             child: Image.asset('assets/images/chat.png'),
+          ),
+          const Text(
+            'Welcome to FastChat',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const Text('Enter your phone number to continue'),
+
+          const SizedBox(height: 20,),
+
+          Form(
+            child: TextFormField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'Enter your phone number',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                )
+              ),
+            ),
           ),
         ],
       ),
