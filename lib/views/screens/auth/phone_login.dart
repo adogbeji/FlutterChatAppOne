3f2db01961286cd11b669c1f3c8782df0723efba
adogbeji <baka_1993@yahoo.co.uk>
2025-03-29 09:20:29 +0000

    Wrap Padding around phone login screen & add colors.dart file

--- lib/views/screens/auth/phone_login.dart
+++ lib/views/screens/auth/phone_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chat_app/utils/constants/colors.dart';
 
 class PhoneLogin extends StatefulWidget {
   const PhoneLogin({super.key});
@@ -18,48 +19,54 @@ class _PhoneLoginState extends State<PhoneLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Image.asset('assets/images/chat.png'),
-          ),
-          const Text(
-            'Welcome to FastChat',
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.w700,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Image.asset('assets/images/chat.png', fit: BoxFit.cover,),
             ),
-          ),
-          const Text('Enter your phone number to continue'),
-
-          const SizedBox(height: 20,),
-
-          Form(
-            key: _formKey_1,
-            child: TextFormField(
-              controller: _phoneNumberController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: 'Enter your phone number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+            const Text(
+              'Welcome to FastChat',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const Text('Enter your phone number to continue'),
+        
+            const SizedBox(height: 10,),
+        
+            Form(
+              key: _formKey_1,
+              child: TextFormField(
+                controller: _phoneNumberController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  labelText: 'Enter your phone number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
               ),
             ),
-          ),
-
-          const SizedBox(height: 20,),
-          SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(),
-              child: const Text('Send OTP'),
-            ),
-          )
-        ],
+        
+            const SizedBox(height: 20,),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kPrimaryColor,
+                  foregroundColor: Colors.white,
+                ),
+                child: const Text('Send OTP'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
